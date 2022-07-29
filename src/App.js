656f5c252ea5e88a260a99c9e6e841a778c98df0
sung-da <desung2020@naver.com>
2022-07-29 16:53:52 +0900

    #6.2 Deps

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import Button from "./Button.js";
-import styles from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setCount((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []);
+
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]); // keyword가 변화할 때 코드를 실행, count가 변화할 때는 실행되지 않음
+
+  useEffect(() => {
+    console.log("I run when 'count' changes.");
+  }, [count]);
+
+  useEffect(() => {
+    console.log("I run when keyword & count change");
+  }, [keyword, count]);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>{count}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }

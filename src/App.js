15748a253d1c6e54928f27e6d9bb432d1ba5b03f
sung-da <desung2020@naver.com>
2022-08-04 16:55:45 +0900

    #6.4 Cleanup

--- src/App.js
+++ src/App.js
@@ -1,37 +1,24 @@
 import { useEffect, useState } from "react";
 
+function Hello() {
+  function byFn() {
+    console.log("bye :(");
+  }
+  function hiFn() {
+    console.log("created :)");
+    return byFn;
+  }
+  useEffect(hiFn, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [count, setCount] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setCount((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-
-  useEffect(() => {
-    console.log("I run only once.");
-  }, []); // dependency가 없기 때문에 처음에 한번만 실행
-
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]); // keyword가 변화할 때만 코드를 실행
-
-  useEffect(() => {
-    console.log("I run when 'count' changes.");
-  }, [count]); // count가 변화할 때만 코드를 실행
-
-  useEffect(() => {
-    console.log("I run when keyword & count change");
-  }, [keyword, count]); // keyword와 count 둘 다 지켜보다가 변할 때 코드 실행
-
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{count}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }

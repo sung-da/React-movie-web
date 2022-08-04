2dcf12e66f1ad12cb2d7f9552731d44e93727b61
sung-da <desung2020@naver.com>
2022-08-04 15:51:39 +0900

    #6.3 Recap

--- src/App.js
+++ src/App.js
@@ -8,19 +8,19 @@ function App() {
 
   useEffect(() => {
     console.log("I run only once.");
-  }, []);
+  }, []); // dependency가 없기 때문에 처음에 한번만 실행
 
   useEffect(() => {
     console.log("I run when 'keyword' changes.");
-  }, [keyword]); // keyword가 변화할 때 코드를 실행, count가 변화할 때는 실행되지 않음
+  }, [keyword]); // keyword가 변화할 때만 코드를 실행
 
   useEffect(() => {
     console.log("I run when 'count' changes.");
-  }, [count]);
+  }, [count]); // count가 변화할 때만 코드를 실행
 
   useEffect(() => {
     console.log("I run when keyword & count change");
-  }, [keyword, count]);
+  }, [keyword, count]); // keyword와 count 둘 다 지켜보다가 변할 때 코드 실행
 
   return (
     <div>
